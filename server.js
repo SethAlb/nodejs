ac865c36307c061a476b058c518016e93945ef34
SethAlb <albergaria.seth213@gmail.com>
2024-12-13 18:50:35 -0500

    Final Video

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send('Hello, Seth A2 Test Node.js App!');
+res.send('Hello, Final  Video A2 Node.js App!');
 });
 app.listen(PORT, () => {
 console.log(`App running on http://localhost:${PORT}`);
