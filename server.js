bcf616be0b38a22c0679d619a806f4f8c965a2fe
SethAlb <albergaria.seth213@gmail.com>
2024-12-13 19:03:11 -0500

    voice over

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send('Hello, Final  Video A2 Node.js App!');
+res.send('A2 bl bla bla Seth  Node.js App!');
 });
 app.listen(PORT, () => {
 console.log(`App running on http://localhost:${PORT}`);
